5f9fd6075bf90b89ec13db35f85421610c0ef086
ThanKarab <tkarabatsis@hotmail.com>
2026-03-19 14:48:25 +0200

    feat: Use object to pass run_udf system args instead of dict

--- exaflow/worker/exareme3/udf/udf_service.py
+++ exaflow/worker/exareme3/udf/udf_service.py
@@ -8,7 +8,6 @@ from exaflow.algorithms.exareme3.longitudinal_transformer import (
 )
 from exaflow.algorithms.exareme3.utils.registry import exareme3_registry
 from exaflow.algorithms.federated.utils import BadInputError
-from exaflow.algorithms.utils.inputdata_utils import Inputdata
 from exaflow.algorithms.utils.pandas_utils import convert_to_pandas_dataframe
 from exaflow.worker import config as worker_config
 from exaflow.worker.exareme3.lazy_aggregation import lazy_agg
@@ -17,6 +16,7 @@ from exaflow.worker.utils.logger import get_logger
 from exaflow.worker.utils.logger import initialise_logger
 from exaflow.worker_communication import BadUserInput
 from exaflow.worker_communication import InsufficientDataError
+from exaflow.worker_communication import RunUdfSystemArgs
 
 
 def enforce_enum_order(data_dict):
@@ -43,7 +43,7 @@ def run_udf(
     request_id,
     udf_registry_key: str,
     kw_args: dict,
-    system_args: dict,
+    system_args: RunUdfSystemArgs,
 ):
     udf = exareme3_registry.get_func(udf_registry_key)
     if not udf:
@@ -59,15 +59,15 @@ def run_udf(
         agg_dns = worker_config.aggregation_server.dns
         agg_client = AggregationClient(request_id, aggregator_dns=agg_dns)
 
-    inputdata = Inputdata.model_validate_json(system_args.get("inputdata"))
+    inputdata = system_args.inputdata
     # GRPC will mess with the order of dict when sending from controller to worker we need a list with the order to we can re-arrange them properly
-    metadata = enforce_enum_order(system_args["metadata"])
+    metadata = enforce_enum_order(system_args.metadata)
     if (
         "metadata" in kw_args
     ):  # TODO We should not expect the metadata hard coded in a specific name, try to remove
         kw_args["metadata"] = enforce_enum_order(kw_args["metadata"])
 
-    preprocessing = system_args.get("preprocessing", None)
+    preprocessing = system_args.preprocessing
     include_dataset = False
     extra_columns = set()
     if preprocessing and "longitudinal_transformer" in preprocessing:
@@ -80,12 +80,12 @@ def run_udf(
 
     data = load_algorithm_arrow_table(
         inputdata,
-        dropna=system_args["drop_na"],
-        include_dataset=(include_dataset or system_args["add_dataset_variable"]),
+        dropna=system_args.drop_na,
+        include_dataset=(include_dataset or system_args.add_dataset_variable),
         extra_columns=extra_columns if extra_columns else None,
     )
 
-    if system_args["check_min_rows"]:
+    if system_args.check_min_rows:
         num_rows = data.num_rows
         min_required = worker_config.privacy.minimum_row_count
         if num_rows < min_required:
